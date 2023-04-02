9b527ab3caa98ea7e2ea2225640feb54eb41bf2c
SakthiMahendran <kingsakthi2005@gmail.com>
2023-04-02 08:27:52 +0530

    Fixing the incorrect variable reference in.

--- statuslogger/logwriter.go
+++ statuslogger/logwriter.go
@@ -17,19 +17,17 @@ func newLogWriter(file *os.File) logWriter {
 // Writes the given bytes to standard output and the log file
 // Returns the number of bytes written and any error encountered
 func (lw *logWriter) Write(p []byte) (n int, err error) {
-	n1, err1 := os.Stdout.Write(p) // write the bytes to standard output and save the number of bytes written and any error
-
-	// if an error was encountered, return the number of bytes written and the error
+	n1, err := os.Stdout.Write(p) // write the bytes to standard output and save the number of bytes written and any error
 	if err != nil {
-		return n1, err1
+		// if an error was encountered, return the number of bytes written and the error
+		return n1, err
 	}
 
 	// write the bytes to the log file and save the number of bytes written and any error
-	n2, err2 := lw.logFile.Write(p)
-
-	// if an error was encountered, return the number of bytes written and the error
+	n2, err := lw.logFile.Write(p)
 	if err != nil {
-		return n2, err2
+		// if an error was encountered, return the number of bytes written and the error
+		return n2, err
 	}
 
 	// return the number of bytes written and nil
